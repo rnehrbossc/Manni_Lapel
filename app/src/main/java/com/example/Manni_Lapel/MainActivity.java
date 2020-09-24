e62ebf40fdf7f13960fd6ffbf3ac66615fd373b8
RichardC <rnehrbossc@gmail.com>
2020-09-24 17:41:34 -0500

    second

--- app/src/main/java/com/example/Manni_Lapel/MainActivity.java
+++ app/src/main/java/com/example/Manni_Lapel/MainActivity.java
@@ -201,7 +201,7 @@ public class MainActivity extends Activity {
         Toast.makeText(MainActivity.this, source.length()+"", Toast.LENGTH_SHORT).show();
 
 
-        //Gets the files to look  at:
+        //Gets the files to lookat:
         String filepath = Environment.getExternalStorageDirectory().getPath();
         File file = new File(filepath,AUDIO_RECORDER_FOLDER);
 
